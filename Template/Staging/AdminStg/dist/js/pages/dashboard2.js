56117c8b1636aac2f9099b55502ef66ab7ba1307
dashayushman <dash.ayushman.99@gmail.com>
2016-01-17 22:02:17 +0100

    Compare page and upload page added

--- Template/Staging/AdminStg/dist/js/pages/dashboard2.js
+++ Template/Staging/AdminStg/dist/js/pages/dashboard2.js
@@ -21,25 +21,24 @@ $(function () {
     datasets: [
       {
         label: "Electronics",
-        fillColor: "rgb(60,141,188)",
+        fillColor: "rgb(210, 214, 222)",
         strokeColor: "rgb(210, 214, 222)",
         pointColor: "rgb(210, 214, 222)",
         pointStrokeColor: "#c1c7d1",
         pointHighlightFill: "#fff",
         pointHighlightStroke: "rgb(220,220,220)",
         data: [65, 59, 80, 81, 56, 55, 40]
+      },
+      {
+        label: "Digital Goods",
+        fillColor: "rgba(60,141,188,0.9)",
+        strokeColor: "rgba(60,141,188,0.8)",
+        pointColor: "#3b8bba",
+        pointStrokeColor: "rgba(60,141,188,1)",
+        pointHighlightFill: "#fff",
+        pointHighlightStroke: "rgba(60,141,188,1)",
+        data: [28, 48, 40, 19, 86, 27, 90]
       }
-//        ,
-//      {
-//        label: "Digital Goods",
-//        fillColor: "rgba(60,141,188,0.9)",
-//        strokeColor: "rgba(60,141,188,0.8)",
-//        pointColor: "#3b8bba",
-//        pointStrokeColor: "rgba(60,141,188,1)",
-//        pointHighlightFill: "#fff",
-//        pointHighlightStroke: "rgba(60,141,188,1)",
-//        data: [28, 48, 40, 19, 86, 27, 90]
-//      }
     ]
   };
 
@@ -74,7 +73,6 @@ $(function () {
     datasetStrokeWidth: 2,
     //Boolean - Whether to fill the dataset with a color
     datasetFill: true,
-    multiTooltipTemplate: "<%= datasetLabel %> - <%= value %>",
     //String - A legend template
     legendTemplate: "<ul class=\"<%=name.toLowerCase()%>-legend\"><% for (var i=0; i<datasets.length; i++){%><li><span style=\"background-color:<%=datasets[i].lineColor%>\"></span><%=datasets[i].label%></li><%}%></ul>",
     //Boolean - whether to maintain the starting aspect ratio or not when responsive, if set to false, will take up entire container
@@ -94,75 +92,75 @@ $(function () {
   //- PIE CHART -
   //-------------
   // Get context with jQuery - using jQuery's .get() method.
-  var pieChartCanvas = $("#pieChart").get(0).getContext("2d");
-  var pieChart = new Chart(pieChartCanvas);
-  var PieData = [
-    {
-      value: 700,
-      color: "#f56954",
-      highlight: "#f56954",
-      label: "Chrome"
-    },
-    {
-      value: 500,
-      color: "#00a65a",
-      highlight: "#00a65a",
-      label: "IE"
-    },
-    {
-      value: 400,
-      color: "#f39c12",
-      highlight: "#f39c12",
-      label: "FireFox"
-    },
-    {
-      value: 600,
-      color: "#00c0ef",
-      highlight: "#00c0ef",
-      label: "Safari"
-    },
-    {
-      value: 300,
-      color: "#3c8dbc",
-      highlight: "#3c8dbc",
-      label: "Opera"
-    },
-    {
-      value: 100,
-      color: "#d2d6de",
-      highlight: "#d2d6de",
-      label: "Navigator"
-    }
-  ];
-  var pieOptions = {
-    //Boolean - Whether we should show a stroke on each segment
-    segmentShowStroke: true,
-    //String - The colour of each segment stroke
-    segmentStrokeColor: "#fff",
-    //Number - The width of each segment stroke
-    segmentStrokeWidth: 1,
-    //Number - The percentage of the chart that we cut out of the middle
-    percentageInnerCutout: 50, // This is 0 for Pie charts
-    //Number - Amount of animation steps
-    animationSteps: 100,
-    //String - Animation easing effect
-    animationEasing: "easeOutBounce",
-    //Boolean - Whether we animate the rotation of the Doughnut
-    animateRotate: true,
-    //Boolean - Whether we animate scaling the Doughnut from the centre
-    animateScale: false,
-    //Boolean - whether to make the chart responsive to window resizing
-    responsive: true,
-    // Boolean - whether to maintain the starting aspect ratio or not when responsive, if set to false, will take up entire container
-    maintainAspectRatio: false,
-    //String - A legend template
-    legendTemplate: "<ul class=\"<%=name.toLowerCase()%>-legend\"><% for (var i=0; i<segments.length; i++){%><li><span style=\"background-color:<%=segments[i].fillColor%>\"></span><%if(segments[i].label){%><%=segments[i].label%><%}%></li><%}%></ul>",
-    //String - A tooltip template
-    tooltipTemplate: "<%=value %> <%=label%> users"
-  };
-  //Create pie or douhnut chart
-  // You can switch between pie and douhnut using the method below.
-  pieChart.Doughnut(PieData, pieOptions);
+  //var pieChartCanvas = $("#pieChart").get(0).getContext("2d");
+  // var pieChart = new Chart(pieChartCanvas);
+  // var PieData = [
+  //   {
+  //     value: 700,
+  //     color: "#f56954",
+  //     highlight: "#f56954",
+  //     label: "Chrome"
+  //   },
+  //   {
+  //     value: 500,
+  //     color: "#00a65a",
+  //     highlight: "#00a65a",
+  //     label: "IE"
+  //   },
+  //   {
+  //     value: 400,
+  //     color: "#f39c12",
+  //     highlight: "#f39c12",
+  //     label: "FireFox"
+  //   },
+  //   {
+  //     value: 600,
+  //     color: "#00c0ef",
+  //     highlight: "#00c0ef",
+  //     label: "Safari"
+  //   },
+  //   {
+  //     value: 300,
+  //     color: "#3c8dbc",
+  //     highlight: "#3c8dbc",
+  //     label: "Opera"
+  //   },
+  //   {
+  //     value: 100,
+  //     color: "#d2d6de",
+  //     highlight: "#d2d6de",
+  //     label: "Navigator"
+  //   }
+  // ];
+  // var pieOptions = {
+  //   //Boolean - Whether we should show a stroke on each segment
+  //   segmentShowStroke: true,
+  //   //String - The colour of each segment stroke
+  //   segmentStrokeColor: "#fff",
+  //   //Number - The width of each segment stroke
+  //   segmentStrokeWidth: 1,
+  //   //Number - The percentage of the chart that we cut out of the middle
+  //   percentageInnerCutout: 50, // This is 0 for Pie charts
+  //   //Number - Amount of animation steps
+  //   animationSteps: 100,
+  //   //String - Animation easing effect
+  //   animationEasing: "easeOutBounce",
+  //   //Boolean - Whether we animate the rotation of the Doughnut
+  //   animateRotate: true,
+  //   //Boolean - Whether we animate scaling the Doughnut from the centre
+  //   animateScale: false,
+  //   //Boolean - whether to make the chart responsive to window resizing
+  //   responsive: true,
+  //   // Boolean - whether to maintain the starting aspect ratio or not when responsive, if set to false, will take up entire container
+  //   maintainAspectRatio: false,
+  //   //String - A legend template
+  //   legendTemplate: "<ul class=\"<%=name.toLowerCase()%>-legend\"><% for (var i=0; i<segments.length; i++){%><li><span style=\"background-color:<%=segments[i].fillColor%>\"></span><%if(segments[i].label){%><%=segments[i].label%><%}%></li><%}%></ul>",
+  //   //String - A tooltip template
+  //   tooltipTemplate: "<%=value %> <%=label%> users"
+  // };
+  // //Create pie or douhnut chart
+  // // You can switch between pie and douhnut using the method below.
+  // pieChart.Doughnut(PieData, pieOptions);
   //-----------------
   //- END PIE CHART -
   //-----------------
